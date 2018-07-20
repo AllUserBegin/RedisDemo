cdd5132939226d297c77cc25abc0ac844ce0d49b
Lic <597153059@qq.com>
2018-07-20 16:22:11 +0800

    tijiao

---
+++ RedisDemo/Models/UserModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedisDemo.Models
+{
+    public class UserModel
+    {
+        public int Uid
+        {
+            get; set;
+        } = 2;
+
+        public string NickName
+        {
+            get; set;
+        } = "NickName";
+
+        public string UserName
+        {
+            get; set;
+        } = "UserName";
+
+        public bool Sex
+        {
+            set;
+            get;
+        } = false;
+
+        public string HeadUrl
+        {
+            set;
+            get;
+        } = "HeadUrl";
+
+        public string PhoneNo
+        {
+            set;
+            get;
+        } = "1379938";
+
+        public string Address
+        {
+            set;
+            get;
+        } = "Address";
+    }
+}
